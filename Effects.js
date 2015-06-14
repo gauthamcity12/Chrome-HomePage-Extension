206ccbe2bea85eee980d2c3e0899fc36230d4a1c
gauthamcity12 <gauthamcity12@gmail.com>
2015-06-14 19:28:50 -0400

    Updated JS and Web Files

--- Effects.js
+++ Effects.js
@@ -2,16 +2,20 @@ $(document).ready(function(){
     $('div.hidden').fadeIn(700).removeClass('hidden');
 });
 
-$('#inputtext').keypress(function(e){
-    if(e.which == 13){
+$('#inputtext').keydown(function(e){
+	console.log("here");
+	alert('key' + e.which);
+	$('#inputtext').focus();
+    if(e.which === 13){
     	alert('You pressed enter!');
         $('#inputtext').click();
     }
 });
 
-$('#inputtext').click(function(){
+$('#searchbutton').click(function(){
 	var location = "http://www.google.com/search?q=" + document.getElementById('#inputtext').value;
 	window.location.replace(location);
+	alert("you got it");
 });
 
 chrome.identity.getProfileUserInfo(function(userInfo){
